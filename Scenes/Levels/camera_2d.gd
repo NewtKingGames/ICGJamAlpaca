99ac56502effa9f652fd7a57cf118ab41ccc2df4
NewtKingGames <newtkinggames@gmail.com>
2025-01-18 20:49:25 -0600

    Add troughs and dirt to game

---
+++ Scenes/Levels/camera_2d.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+
+# TODO - add juice for
+
+# - shake the camera slightly when you get hit by llama spit
+# - shake camera when slime get's through the path
+# - zoom in camera when holding a llama (maybe some shake too?)
+
